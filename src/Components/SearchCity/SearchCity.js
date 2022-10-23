187a2526ed98d5dc4cfa58192652c3c84ee14c1c
iryna-zatynina <ira.zat1997@gmail.com>
2022-10-23 15:40:29 +0200

    add logic

--- src/Components/SearchCity/SearchCity.js
+++ src/Components/SearchCity/SearchCity.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchCity.scss'
 import {useTranslation} from "react-i18next";
 
-const SearchCity = () => {
-    const {t} = useTranslation()
+const SearchCity = ({city, handleCity, getForecast, message}) => {
+    const {t} = useTranslation();
+
     return (
         <div className='container'>
             <form className='SearchCity'>
-                <input className='SearchCity__input' type="text" placeholder={t("Enter your city")}/>
-                <button className='SearchCity__btn'>{t("Search")}</button>
+                <input
+                    className='SearchCity__input'
+                    type="text"
+                    placeholder={t("Enter your city")}
+                    value={city}
+                    onChange={handleCity}/>
+                <button className='SearchCity__btn' onClick={getForecast}>{t("Search")}</button>
             </form>
+            <p>{message}</p>
         </div>
     );
 };
--- src/Pages/Forecast/Forecast.js
+++ src/Pages/Forecast/Forecast.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../../Components/Header/Header";
 import SearchCity from "../../Components/SearchCity/SearchCity";
 import ForecastInfo from "../../Components/ForecastInfo/ForecastInfo";
+import axios from "axios";
 
 const Forecast = () => {
+    useEffect(() => {
+        axios.get('https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=Kharkiv')
+            .then((response) => {
+                const info = response.data;
+                setForecastInfo({
+                    name: info.name,
+                    icon: "",
+                    temp: Math.round(info.main.temp),
+                    feel: Math.round(info.main.feels_like),
+                    pressure: info.main.pressure,
+                    humidity: info.main.humidity,
+                    wind: info.wind.speed,
+                })
+            })
+    }, [])
+    const [city, setCity] = useState('');
+    const [message, setMessage] = useState('')
+    const [forecastInfo, setForecastInfo] = useState({
+        name: '',
+        icon: '',
+        temp: '',
+        feel: '',
+        pressure: '',
+        humidity: '',
+        wind: '',
+    });
+
+    const handleCity = (e) => {
+        setMessage("");
+        setCity(e.target.value);
+    }
+    const getForecast = (e) => {
+        e.preventDefault();
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=${city}`)
+            .then((response) => {
+                const info = response.data;
+                setForecastInfo({
+                    name: info.name,
+                    icon: "",
+                    temp: Math.round(info.main.temp),
+                    feel: Math.round(info.main.feels_like),
+                    pressure: info.main.pressure,
+                    humidity: info.main.humidity,
+                    wind: info.wind.speed,
+                })
+            })
+            .catch(() => {
+                setMessage("There is no such city");
+            })
+        setCity('');
+    }
     return (
         <div>
-            <Header />
-            <SearchCity />
-            <ForecastInfo />
+            <Header/>
+            <SearchCity city={city} handleCity={handleCity} getForecast={getForecast} message={message}/>
+            <ForecastInfo forecastInfo={forecastInfo}/>
         </div>
     );
 };
