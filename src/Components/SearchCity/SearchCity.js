9c86941ecc85bffe681950629f9a759949257002
iryna-zatynina <ira.zat1997@gmail.com>
2022-10-24 14:33:59 +0200

    add fix

--- src/Components/SearchCity/SearchCity.js
+++ src/Components/SearchCity/SearchCity.js
@@ -2,9 +2,14 @@ import React, {useState} from 'react';
 import './SearchCity.scss'
 import {useTranslation} from "react-i18next";
 
-const SearchCity = ({city, handleCity, getForecast, message}) => {
+const SearchCity = ({city, setCity, handleCity, getForecast, message, validation}) => {
     const {t} = useTranslation();
-
+    const onSearchClick = (e) => {
+        e.preventDefault();
+        if (validation) {
+            getForecast();
+        }
+    }
     return (
         <div className='container'>
             <form className='SearchCity'>
@@ -14,9 +19,9 @@ const SearchCity = ({city, handleCity, getForecast, message}) => {
                     placeholder={t("Enter your city")}
                     value={city}
                     onChange={handleCity}/>
-                <button className='SearchCity__btn' onClick={getForecast}>{t("Search")}</button>
+                <button className='SearchCity__btn' onClick={onSearchClick}>{t("Search")}</button>
             </form>
-            <p>{message}</p>
+            <p className="validation-message">{t(message)}</p>
         </div>
     );
 };
--- src/Pages/Forecast/Forecast.js
+++ src/Pages/Forecast/Forecast.js
@@ -5,61 +5,65 @@ import ForecastInfo from "../../Components/ForecastInfo/ForecastInfo";
 import axios from "axios";
 
 const Forecast = () => {
+
     useEffect(() => {
         axios.get('https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=Kharkiv')
             .then((response) => {
-                const info = response.data;
-                setForecastInfo({
-                    name: info.name,
-                    icon: "",
-                    temp: Math.round(info.main.temp),
-                    feel: Math.round(info.main.feels_like),
-                    pressure: info.main.pressure,
-                    humidity: info.main.humidity,
-                    wind: info.wind.speed,
-                })
+                setData(response);
             })
     }, [])
-    const [city, setCity] = useState('');
+
+    const [forecastInfo, setForecastInfo] = useState({});
+    const [city, setCity] = useState("");
     const [message, setMessage] = useState('')
-    const [forecastInfo, setForecastInfo] = useState({
-        name: '',
-        icon: '',
-        temp: '',
-        feel: '',
-        pressure: '',
-        humidity: '',
-        wind: '',
-    });
 
     const handleCity = (e) => {
         setMessage("");
         setCity(e.target.value);
+        validation(e);
     }
-    const getForecast = (e) => {
-        e.preventDefault();
+    const getForecast = () => {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=${city}`)
             .then((response) => {
-                const info = response.data;
-                setForecastInfo({
-                    name: info.name,
-                    icon: "",
-                    temp: Math.round(info.main.temp),
-                    feel: Math.round(info.main.feels_like),
-                    pressure: info.main.pressure,
-                    humidity: info.main.humidity,
-                    wind: info.wind.speed,
-                })
+                setData(response);
             })
             .catch(() => {
-                setMessage("There is no such city");
+                if (city.length > 0) {
+                    setMessage("There is no such city");
+                } else {
+                    setMessage('Enter your city')
+                }
             })
         setCity('');
+    }
+
+    const setData = (result) => {
+        const info = result.data;
+        setForecastInfo({
+            name: info.name,
+            icon: "",
+            temp: Math.round(info.main.temp),
+            feel: Math.round(info.main.feels_like),
+            pressure: info.main.pressure,
+            humidity: info.main.humidity,
+            wind: info.wind.speed,
+        })
+    }
+
+    const validation = (e) => {
+        const regex = /[a-zA-Z]+$/g;
+        if (!regex.test(e.target.value) && e.target.value.length > 0) {
+            setMessage("Enter latin letters")
+            return false
+        } else {
+            setMessage("")
+            return true
+        }
     }
     return (
         <div>
             <Header/>
-            <SearchCity city={city} handleCity={handleCity} getForecast={getForecast} message={message}/>
+            <SearchCity city={city} setCity={setCity} handleCity={handleCity} getForecast={getForecast} message={message} validation={validation}/>
             <ForecastInfo forecastInfo={forecastInfo}/>
         </div>
     );
